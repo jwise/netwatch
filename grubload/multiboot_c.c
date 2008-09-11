60a917ef0909a3d22904e8204a0df64094af6e50
Joshua Wise <joshua@rebirth.joshuawise.com>
2008-09-10 21:44:55 -0400

    Load into SMRAM, not just video RAM.

--- grubload/multiboot_c.c
+++ grubload/multiboot_c.c
@@ -1,4 +1,5 @@
 #include "console.h"
+#include <io.h>
 
 extern char _binary_realmode_bin_start[];
 extern int _binary_realmode_bin_size;
@@ -24,6 +25,7 @@ struct mod_info
 void c_start(unsigned int magic, struct mb_info *wee)
 {
 	unsigned short *grubptr = 0x7CFE;
+	unsigned char smramc;
 	int i;
 	
 	void (*realmode)() = 0x4000;
@@ -48,13 +50,37 @@ void c_start(unsigned int magic, struct mb_info *wee)
 		puts("  Name: "); puts(wee->mods[i].mod_string); puts("\n");
 	}
 
-	if ((wee->mod_cnt != 1) || (strcmp(wee->mods[0].mod_string, "aseg.elf")))
+	if (wee->mod_cnt != 1)
 	{
-		puts("Expected 1 module called aseg.elf.\n");
+		puts("Expected exactly one module; cannot continue.\n");
 		while(1) asm("hlt");
 	}
 
+	puts("Current USB state is: "); puthex(pci_read16(0, 31, 2, 0xC0)); puts(" "); puthex(pci_read16(0, 31, 4, 0xC0)); puts("\n");
+	puts("Current SMI state is: "); puthex(inl(0x830)); puts("\n");
+	puts("Current SMRAMC state is: "); puthex(pci_read8(0, 0, 0, 0x70)); puts("\n");
+	
+	outl(0x830, inl(0x830) & ~0x2001);	/* turn off SMIs */
+	
+	/* Try really hard to shut up USB_LEGKEY. */
+	pci_write16(0, 31, 2, 0xC0, pci_read16(0, 31, 2, 0xC0));
+	pci_write16(0, 31, 2, 0xC0, 0);
+	pci_write16(0, 31, 4, 0xC0, pci_read16(0, 31, 4, 0xC0));
+	pci_write16(0, 31, 4, 0xC0, 0);
+	smramc = pci_read8(0, 0, 0, 0x70);
+	pci_write8(0, 0, 0, 0x70, (smramc & 0xF0) | 0x04);
 	load_elf(wee->mods[0].mod_start, wee->mods[0].mod_end - wee->mods[0].mod_start);
+	pci_write8(0, 0, 0, 0x70, smramc);
+	outb(0x830, inb(0x830) | 0x41);	/* turn on the SMIs we want */
+	
+	puts("Waiting for a bit before returning to real mode...");
+	for (i=0; i<0x500000; i++)
+	{
+		if ((i % 0x100000) == 0)
+			puts(".");
+		inb(0x80);
+	}
+	puts("\n");
 
 	puts("Now returning to real mode.\n");	
 	memcpy(0x4000, _binary_realmode_bin_start, (int)&_binary_realmode_bin_size);
