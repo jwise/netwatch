4122ee13ba211a913b04906c39e72e512cb15c08
Joshua Wise <joshua@rebirth.joshuawise.com>
2008-09-13 01:23:13 -0400

    Reboot the machine when you hit escape. Wait, what?

--- aseg/counter.c
+++ aseg/counter.c
@@ -78,6 +78,54 @@ void __start (void)
 	
 	if (inl(0x834) & 0x20)
 		dolog("Warning: unhandled APM access");
+	if (inl(0x834) & 0x1000)
+	{
+		if (inl(0x844) & 0x1000)	/* devact_sts */
+		{
+			unsigned char s[40];
+			unsigned long cts;
+			static int curdev = 0;	/* 0 if kbd, 1 if mouse */
+			
+			cts = inl(0x84C);
+			
+			outl(0x848, 0x0);
+			
+			switch(cts&0xF0000)
+			{
+			case 0x20000:
+			{
+				unsigned char b;
+				strcpy(s, "READxxxxxxxxxxxxxxxx");
+				tohex(s+4, cts);
+				b = inb(cts & 0xFFFF);
+				tohex(s+12, b);
+				if ((cts & 0xFFFF) == 0x64)
+					curdev = (b & 0x20) ? 1 : 0;
+				if ((curdev == 0) && ((cts & 0xFFFF) == 0x60) && (b == 0x01))
+					outb(0xCF9, 0x4);
+				dolog(s);
+				*(unsigned char*)0xAFFD0 /* EAX */ = b;
+				break;
+			}
+			case 0x30000:
+			{
+				unsigned char b;
+				
+				strcpy(s, "WRITxxxxxxxxxxxxxxxx");
+				b = *(unsigned char*)0xAFFD0 /* EAX */;
+				tohex(s+4, cts);
+				tohex(s+12, b);
+				dolog(s);
+				outb(cts & 0xFFFF, b);
+				break;
+			}
+			default:
+				dolog("Unhandled PCI cycle");
+			}
+			outl(0x848, 0x1000);
+			outl(0x844, 0x1000);
+		}
+	}
 	if (inl(0x834) & 0x4000)
 		dolog("Long periodic timer");
 	if (inl(0x834) & ~(0x4160))
@@ -93,6 +141,7 @@ void __start (void)
 	outl(0xCF8, pcisave);
 	outb(0x3D4, vgasave);
 	
+	outl(0x848, 0x1000);
 	outl(0x834, /*0x40*/0xFFFF);	// ack the periodic IRQ
 	outb(0x830, (inb(0x830) | 0x2) & ~0x40);
 	outb(0x830, inb(0x830) | 0x40);
