748534f490f9da36041ad8e2ecc046c6a8238d81
Jacob Potter <jacob@J4CBO-MBP-R7.RES.CMU.EDU>
2008-10-02 19:01:41 -0400

    add net

---
+++ net/etherboot-compat.h
@@ -0,0 +1,18 @@
+#define ETH_ALEN	6
+
+struct dev {
+	void (*disable) (struct dev *dev);
+};
+
+struct nic {
+	char * packet;
+	int packetlen;
+
+	unsigned int ioaddr;
+	unsigned int irqno;
+
+	int (*poll) (struct nic *nic, int retrieve);
+	void (*transmit) (const char *dest_addr, unsigned int proto, unsigned int size, const char *pkt);
+};
+
+#define virt_to_bus(x) ((unsigned long)x)
