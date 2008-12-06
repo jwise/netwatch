2d34b5d2cb94b7976fa40e954bcf060bdb5856c2
Joshua Wise <joshua@nyus.joshuawise.com>
2008-12-06 06:09:58 -0500

    Manage to get a nice big packet queue going inside lwIP.

--- lwip/src/include/lwipopts.h
+++ lwip/src/include/lwipopts.h
@@ -11,10 +11,17 @@
 
 #define LWIP_DHCP	1
 
+/* For big ones... */
+extern void _memcpy(void *dest, const void *src, int bytes);
+#define MEMCPY(dst,src,len) _memcpy(dst,src,len)
+
+/* Lots of tricks from http://lists.gnu.org/archive/html/lwip-users/2006-11/msg00007.html */
+
 #define MEM_SIZE	65536
-#define TCP_MSS         1400
+#define TCP_MSS         1460
 #define TCP_WND		24000
 #define TCP_SND_BUF     (16 * TCP_MSS)
+#define TCP_SND_QUEUELEN 16
 
 #define MEMP_NUM_PBUF	128
 
