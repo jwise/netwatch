91cc13402921ddb5b3b4e695b036a43c3722e615
Jacob Potter <jdpotter@andrew.cmu.edu>
2008-12-12 17:32:00 -0500

    make keyboard handler use keysyms

--- aseg-paging/keyboard.h
+++ aseg-paging/keyboard.h
@@ -1,7 +1,9 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-void kbd_inject_key(unsigned char k);
+#include <stdint.h>
+
+void kbd_inject_keysym(uint32_t k, int downflag);
 
 unsigned char kbd_get_injected_scancode();
 int kbd_has_injected_scancode();
