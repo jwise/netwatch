7e946b3ca6587d82be1b900ee83a23a1caf7d77e
Joshua Wise <joshua@rebirth.joshuawise.com>
2008-12-14 19:26:14 -0500

    Have text.c take scrolling into account.

--- video/text.c
+++ video/text.c
@@ -6,29 +6,52 @@
 #include <output.h>
 #include <smram.h>
 #include <crc32.h>
+#include <video_defines.h>
 
 static unsigned char _font[256 * 32];
+
+/* XXX reunify this with vga-overlay? */
+#define VRAM_BASE		0xA0000UL
+#define TEXT_CONSOLE_OFFSET	0x18000UL 
+
+#define TEXT_CONSOLE_BASE	(VRAM_BASE + TEXT_CONSOLE_OFFSET)
+
+static unsigned char vga_read(unsigned char idx)
+{
+	outb(CRTC_IDX_REG, idx);
+	return inb(CRTC_DATA_REG);
+}
+
+static char * vga_base()
+{
+	return (char *) (
+		TEXT_CONSOLE_BASE
+		+ (((unsigned int) vga_read(CRTC_START_ADDR_MSB_IDX)) << 9)
+		+ (((unsigned int) vga_read(CRTC_START_ADDR_LSB_IDX)) << 1)
+	);
+}
 
 /* Must be called from a firstrun context, where we don't care about saving
  * 0x3CE state. */
 void text_init()
 {
 	unsigned char oldread;
+
 	smram_state_t old_state = smram_save_state();
-	outb(0x3CE, 0x05 /* Mode register */);
-	outb(0x3CF, inb(0x3CF) & ~(0x10 /* Odd/even */));
 	outb(0x3CE, 0x04 /* Read register */);
 	oldread = inb(0x3CF);
 	outb(0x3CF, 0x02 /* Font plane */);
+
 	smram_aseg_set_state(SMRAM_ASEG_SMMCODE);
 	memcpy(_font, p2v(0xB8000), sizeof(_font));
 	smram_restore_state(old_state);
+
 	outb(0x3CF, oldread);
 }
 
 void text_render(char *buf, int x, int y, int w, int h)
 {
-	unsigned char *video = (unsigned char *)0xB8000;
+	unsigned char *video = (unsigned char *)vga_base();
 	unsigned int textx = x / 9;
 	unsigned int texty = y / 14;
 	unsigned int cx, cy;
@@ -77,7 +100,7 @@ void text_render(char *buf, int x, int y, int w, int h)
 
 uint32_t text_checksum(int x, int y, int w, int h)
 {
-	unsigned char *video = (unsigned char *)0xB8000;
+	unsigned char *video = (unsigned char *)vga_base();
 	unsigned int textx = x / 9;
 	unsigned int texty = y / 14;
 	int cx, cy;
