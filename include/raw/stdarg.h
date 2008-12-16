709f319b64f0233f9e60faab2c2b885d3ddbad71
Jacob Potter <jdpotter@andrew.cmu.edu>
2008-12-16 15:23:54 -0500

    better licensing on nwf's stdarg.h

--- include/raw/stdarg.h
+++ include/raw/stdarg.h
@@ -1,6 +1,30 @@
 #ifndef _STDARG_H_
 #define _STDARG_H_
 
+/* stdarg.h
+ * Copyright (c) 2008, Wes Filardo.
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as reproduced below:
+ *
+ *        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                   Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar
+ *  14 rue de Plaisance, 75014 Paris, France
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO. 
+ *
+ */
+
 /* This is awful, but really these are compiler intrinsics, so we use the
  * GNU compiler intrinsics.
  */
